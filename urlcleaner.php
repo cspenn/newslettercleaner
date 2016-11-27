a76d0b06ff118f65ac621de9a2c52d2011b6abec
Christopher Penn <cspenn@gmail.com>
2016-11-27 15:39:47 -0500

    updated to add common cleaner

--- urlcleaner.php
+++ urlcleaner.php
@@ -2,11 +2,10 @@
 
 /***********************************************
 
-Bitly newsletter cleaner
+Newsletter Cleaner and Preparation Script
 Copyright © 2016-2017 Christopher S. Penn
 
-The purpose of this code is to take a list of shortened bit.ly or other redirected URLs, lengthen them, chop off any existing UTM codes and other query parameters to produce a clean URL, then append new UTM codes to it and re-shorten with bit.ly. The output is a CSV, pipe-delimited.
-
+The purpose of this code is to take a list of URLs and expand them if shortened, trim off analytics tracking tags, then add new tags, re-shorten, and append with HTML page titles into Markdown format for use on Github and in any Markdown-compliant writing software.
 Version 2.0
 
 What's New:
@@ -43,7 +42,7 @@ $source   = "YOUR SOURCE";
 $medium   = "YOUR MEDIUM";
 $campaign = "YOUR CAMPAIGN" . $shortstamp; // shortstamp appends a yyyy-mm-dd timestamp
 
-// Page Title Getter Function
+// Page Title Getter
 
 function file_get_contents_curl($url)
 {
@@ -60,10 +59,44 @@ function file_get_contents_curl($url)
     return $data;
 }
 
+// Page Title Cleaner
+
+function cleanuptitles($text)
+{
+    
+    // known issues
+    
+    $text = str_replace(" - Christopher S. Penn Blog", "", $text);
+    $text = str_replace(" - SHIFT Communications PR Agency - Boston | New York | San Francisco | Austin", "", $text);
+    $text = str_replace(" – Medium", "", $text);
+    
+    // common delimiters
+    
+    $text = str_replace(" - ", " via ", $text);
+    $text = str_replace(" | ", " via ", $text);
+    $text = str_replace(" – ", " via ", $text);
+    $text = str_replace(" · ", " via ", $text);
+    $text = str_replace("  ", " ", $text);
+    
+    // clean up artifacts
+    
+    $text = str_replace(". via ", " via ", $text);
+    return $text;
+    
+    // fix ascii
+    
+    $text = mb_convert_encoding($text, "ASCII");
+    
+}
+
 // input file to open
 
 $handle = @fopen("YOUR INPUT FILE HERE.txt", "r");
 
+// parse the file, line by line
+
+if ($handle) {
+    while (($buffer = fgets($handle, 4096)) !== false) {
 // parse the file, line by line
 
 if ($handle) {
@@ -116,7 +149,9 @@ if ($handle) {
                 $keywords = $meta->getAttribute('content');
         }
         
-        $pagetitle = trim($title);
+        $rawtitle  = trim($title);
+        $pagetitle = cleanuptitles($rawtitle);
+        
         
         // re-encode
         $bitly = "https://api-ssl.bitly.com/v3/shorten?&access_token=$apikey&longUrl=$longurl";
@@ -128,7 +163,8 @@ if ($handle) {
         // fwrite($fp, "$url\t$longurl\t$theurl\t$pagetitle\n");
         
         // new format for markdown
-        fwrite($fp, "[$pagetitle]($theurl) : $description\n");
+        // expanded version with descriptions - fwrite($fp, "[$pagetitle]($theurl) : $description\n");
+        fwrite($fp, "[$pagetitle]($theurl)\n");
         
     }
     if (!feof($handle)) {
